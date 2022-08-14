f04204c5f970c0f46e4da0855a8c37fc79fa4d14
YoonHyukJun <gurwns1540@gmail.com>
2022-08-14 23:38:03 +0900

    1. 순환참조 해결 2. Enum 강의 후 자료 조사 후 주석 추가

--- src/main/java/me/whiteship/chapter01/item01/Order.java
+++ src/main/java/me/whiteship/chapter01/item01/Order.java
@@ -27,12 +27,18 @@ public class Order {
         return order;
     }
 
+    // EnumSet, EnumMap을 쓰는 이유
+    /*
+    * 1. Enum은 단일객체(싱글톤패턴)이기 때문에 해싱작업이 필요가 없다.
+    *   - 일반적으로 Set<Enum>, Map<Enum, ?>의 경우 해싱작업이 필요하다
+    * 2. 순서가 보장된다.(TreeMap의 장점을 가지고 있다.)
+    *   - Enum에 명시한 순서가 보장이 된다.
+    * 3. 빠르다.
+    * */
     public static void main(String[] args) {
-
         Order order = new Order();
         if (order.orderStatus == OrderStatus.DELIVERED) {
             System.out.println("delivered");
         }
     }
-
 }
